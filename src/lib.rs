fc48104aff21a3e601cbe0d1c68dbfa78d44d70f
Adam Gregory <adam@aguilaengineering.com>
2026-04-24 14:07:51 +0100

    Re-export user_register_block macro from PAC crate, for convenience

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,9 @@ mod timer;
 mod uart;
 mod user_io;
 
+// Re-export user_register_block macro from PAC crate, for convenience
+pub use caravel_pac::user_register_block;
+
 pub use gpio::Gpio;
 pub use timer::Timer0;
 pub use uart::Uart;
