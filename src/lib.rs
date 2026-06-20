13064877bddf6ec3fa2a1f038de4bcd5708cdc83
Adam Gregory <adam@aguilaengineering.com>
2026-06-20 19:34:32 +0100

    feat: Add Wishobone driver (enable/disable)

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ mod housekeeping_spi;
 mod timer0;
 mod uart;
 mod user_io;
+mod wishbone;
 
 #[cfg(feature = "interrupts")]
 mod interrupts;
@@ -17,6 +18,7 @@ pub use housekeeping_spi::HousekeepingSpi;
 pub use timer0::Timer0;
 pub use uart::Uart;
 pub use user_io::{UserIOBits, UserIo};
+pub use wishbone::Wishbone;
 
 #[cfg(feature = "interrupts")]
 pub use interrupts::{CaravelInterrupt, enable_caravel_interrupts};
